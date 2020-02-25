f1511db1be826ced01d53824b28cb2b1f04d96f9
Jamiex <romari123456789@gmail.com>
2020-02-25 09:57:18 -0600

    Program.cs
    will be continued

---
+++ Program.cs
@@ -0,0 +1,42 @@
+class Menu
+{
+    public int add;
+    public int sub;
+    public int mult;
+    public int div;
+    public int num1;
+    public int num2;
+    public int num3;
+    public int num4;
+    
+    void Addition2()
+    {
+    add=(num1+num2);
+    return add;
+    }
+    void Addition3()
+    {
+    add=(num1+num2+num3);
+    return add;
+    }
+    void Addition4()
+    {
+    add=(num1+num2+num3+num4);
+    return add;
+    }
+    void Addition()
+      {
+      add=(num1+num2+num3+num4+);
+      return add;
+      }
+    void Subtraction2()
+      {
+      sub=(num1+num2);
+      return sub;
+      }
+      void Subtraction3()
+      {
+      sub=(num1+num2+num3);
+      return sub;
+      }
+      
