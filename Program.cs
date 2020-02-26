1b5c18c1e34d27b9f1f66eeab18eb15227638754
Jamiex <romari123456789@gmail.com>
2020-02-26 07:34:07 -0500

    Add switch
    Added the add switch case menu. 
    Note: If statements could have been used instead of switch.

--- Program.cs
+++ Program.cs
@@ -24,11 +24,6 @@ class Menu
     add=(num1+num2+num3+num4);
     return add;
     }
-    void Addition()
-      {
-      add=(num1+num2+num3+num4+);
-      return add;
-      }
     void Subtraction2()
       {
       sub=(num1+num2);
@@ -39,4 +34,49 @@ class Menu
       sub=(num1+num2+num3);
       return sub;
       }
-      
+    void Multi2()
+    {
+        mult=(num1*num2);
+        return mult;
+    }
+    void Multi3()
+    {
+        mult=(num1*num2*num3);
+        return mult;
+    }
+    void Mult4()
+    {
+        mult=(num1*num2*num3*num4);
+        return mult;
+    }
+    void div2()
+    {
+        div=(num1/num2);
+        return div;
+    }
+    void div3()
+    {
+        div=(num1/num2/num3);
+        return div;
+    }
+        public void Add()
+        {
+            int add;
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Hello How many numbers are you trying to Add?  2 , 3 or 4?");
+            add=int.Parse(Console.ReadLine());
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            switch (add)
+            {
+                case 2:
+                    Addition2();
+                    break;
+                case 3:
+                    Addition3();
+                    break;
+                case 4:
+                    Addition4();
+                    break;
+            }
+                    
+                    
