c3479a86cbf7bb03022e55305f6c451c891774db
Jamiex <romari123456789@gmail.com>
2020-02-26 08:24:51 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-class Menu
+namespace Githubmod
+{
+   class Menu
 {
     public int add;
     public int sub;
@@ -12,61 +14,61 @@ class Menu
     void Addition2()
     {
     add=(num1+num2);
-    return add;
+    Console.WriteLine(add);
     }
     void Addition3()
     {
     add=(num1+num2+num3);
-    return add;
+    Console.WriteLine(add);
     }
     void Addition4()
     {
     add=(num1+num2+num3+num4);
-    return add;
+    Console.WriteLine(add);
     }
     void Subtraction2()
       {
       sub=(num1+num2);
-      return sub;
+      Console.WriteLine(sub);
       }
       void Subtraction3()
       {
       sub=(num1+num2+num3);
-      return sub;
+      Console.WriteLine(sub);
       }
     void Multi2()
     {
         mult=(num1*num2);
-        return mult;
+        Console.WriteLine(mult);
     }
     void Multi3()
     {
         mult=(num1*num2*num3);
-        return mult;
+        Console.WriteLine(mult);
     }
     void Mult4()
     {
         mult=(num1*num2*num3*num4);
-        return mult;
+        Console.WriteLine(mult);
     }
     void div2()
     {
         div=(num1/num2);
-        return div;
+        Console.WriteLine(div);
     }
     void div3()
     {
         div=(num1/num2/num3);
-        return div;
+        Console.WriteLine(div);
     }
         public void Add()
         {
-           int addd;
+           int add;
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
             Console.WriteLine("Hello How many numbers are you trying to Add?  2 , 3 or 4?");
             add=int.Parse(Console.ReadLine());
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-            switch (addd)
+            switch (add)
             {
                 case 2:
                     Addition2();
@@ -79,11 +81,12 @@ class Menu
                     break;
             }
         }
-    public void sub()
+    public void subt()
     {
         int subb;
         Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         Console.WriteLine(" Hello, How many numbers are you trying to subract? 2, 3");
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         subb=int.Parse(Console.ReadLine());
         switch (subb)
         {
@@ -95,4 +98,3 @@ class Menu
                 break;
         }
     }
-    
