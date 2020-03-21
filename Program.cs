32c44464d68cffd3afcdce02f610157f2cbb8d03
Jamiex <romari123456789@gmail.com>
2020-03-21 12:36:07 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
-namespace Githubmod
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+
+namespace Openproject
 {
    class Menu
-{
+ {
     public int add;
     public int sub;
     public int mult;
@@ -46,7 +53,7 @@ namespace Githubmod
         mult=(num1*num2*num3);
         Console.WriteLine(mult);
     }
-    void Mult4()
+    void Multi4()
     {
         mult=(num1*num2*num3*num4);
         Console.WriteLine(mult);
@@ -63,7 +70,6 @@ namespace Githubmod
     }
         public void Add()
         {
-           int add;
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
             Console.WriteLine("Hello How many numbers are you trying to Add?  2 , 3 or 4?");
             add=int.Parse(Console.ReadLine());
@@ -71,30 +77,135 @@ namespace Githubmod
             switch (add)
             {
                 case 2:
+                 getdata2();
                     Addition2();
                     break;
                 case 3:
+                 getdata3();
                     Addition3();
                     break;
                 case 4:
+                 getdata4();
                     Addition4();
                     break;
             }
         }
-    public void subt()
+    public void subtract()
     {
-        int subb;
         Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         Console.WriteLine(" Hello, How many numbers are you trying to subract? 2, 3");
         Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        subb=int.Parse(Console.ReadLine());
-        switch (subb)
+        sub=int.Parse(Console.ReadLine());
+        switch (sub)
         {
             case 2:
+             getdata2();
                 Subtraction2();
                 break;
             case 3:
+             getdata3();
                 Subtraction3();
                 break;
         }
     }
+       void getdata2()
+     {
+       Console.WriteLine("Please enter the first number");
+       num1=int.Parse(Console.ReadLine());
+      Console.WriteLine("Please enter the second number");
+       num2=int.Parse(Console.ReadLine());
+       }
+       void getdata3()
+     {
+       Console.WriteLine("Please enter the first number");
+       num1=int.Parse(Console.ReadLine());
+      Console.WriteLine("Please enter the second number");
+       num2=int.Parse(Console.ReadLine());
+       Console.WriteLine("Please enter the third number");
+       num3=int.Parse(Console.ReadLine());
+       }
+       void getdata4()
+     {
+       Console.WriteLine("Please enter the first number");
+       num1=int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the second number");
+       num2=int.Parse(Console.ReadLine());
+       Console.WriteLine("Please enter the third number");
+       num3=int.Parse(Console.ReadLine());
+       Console.WriteLine("Please enter the fourth number");
+       num4=int.Parse(Console.ReadLine());
+       }
+         
+      public void Multiply()
+     {
+       Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Console.WriteLine(" Hello, How many numbers are you trying to Multiply 2, 3 or 4");
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        mult=int.Parse(Console.ReadLine());
+        switch (mult)
+        {
+            case 2:
+             getdata2();
+                Multi2();
+                break;
+            case 3:
+             getdata2();
+                Multi3();
+                break;
+            case 4:
+             getdata4();
+                Multi4();
+                break;
+            
+        }
+    }
+     public void Divide()
+     {
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Console.WriteLine(" Hello, How many numbers are you trying to Divide 2, 3");
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        div=int.Parse(Console.ReadLine());
+       switch (div)
+        {
+            case 2:
+            getdata2();
+                div2();
+                break;
+            case 3:
+            getdata3();
+                div3();
+                break;
+       }
+     }
+     class Pro
+     {
+       
+       static void Main(String []args)
+       {
+         string Choice;
+         Menu MenuProgram = new Menu();
+         
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        Console.WriteLine(" Hello, Are you trying to Multiply, Divide, Add, subract numbers?");
+        Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+         Choice=Console.ReadLine();
+        switch (Choice)
+        {
+          case "Multiply":
+            MenuProgram.Multiply();
+            break;
+          case "Divide":
+            MenuProgram.Divide();
+            break;
+          case "Add":
+            MenuProgram.Add();
+            break;
+          case "subract":
+            MenuProgram.subtract();
+            break;
+        }
+       }
+     }
+   }
+}
+}
